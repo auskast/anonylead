fd396aadf841fe674b9e1097cf8beac90a671027
Patrick Fitzgerald <pfitzgerald@emdunds.com>
2012-08-17 11:26:05 -0700

    hook up simple rest calls

--- main/anonylead-web/src/main/java/com/edmunds/anonylead/client/DemoView.java
+++ main/anonylead-web/src/main/java/com/edmunds/anonylead/client/DemoView.java
@@ -2,11 +2,17 @@ package com.edmunds.anonylead.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Created with IntelliJ IDEA. User: ehlee Date: 8/17/12 Time: 10:32 AM To change this template use File | Settings |
@@ -48,17 +54,57 @@ public class DemoView implements Viewable {
 
         panel.add(getRealEmailPanel);
         final Label result2 = new Label();
+        panel.add(result2);
+
         submitForTempEmail.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                result1.setText("The temporary email for '" + tempEmail.getText() + "' is: ");
                 result2.setText("");
+                if(realEmail.getText().length() == 0) {
+                    return;
+                }
+                final RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, "api/anonyLead/generate/" +
+                    realEmail.getText());
+
+                try {
+                    requestBuilder.sendRequest(null, new RequestCallback() {
+                        public void onResponseReceived(Request request, Response response) {
+                            result1.setText("The temporary email for '" + realEmail.getText() + "' is: " +
+                                response.getText());
+                        }
+
+                        public void onError(Request request, Throwable throwable) {
+                            result1.setText("Error");
+                        }
+                    });
+                } catch(RequestException e) {
+                    result1.setText("Error");
+                }
             }
         });
 
         submitForRealEmail.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                result2.setText("The real email for '" + tempEmail.getText() + "' is: ");
                 result1.setText("");
+                if(tempEmail.getText().length() == 0) {
+                    return;
+                }
+                final RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, "api/anonyLead/lookup/" +
+                    tempEmail.getText());
+
+                try {
+                    requestBuilder.sendRequest(null, new RequestCallback() {
+                        public void onResponseReceived(Request request, Response response) {
+                            result2.setText("The real email for '" + tempEmail.getText() + "' is: " +
+                                response.getText());
+                        }
+
+                        public void onError(Request request, Throwable throwable) {
+                            result2.setText("Error");
+                        }
+                    });
+                } catch(RequestException e) {
+                    result2.setText("Error");
+                }
             }
         });
 
